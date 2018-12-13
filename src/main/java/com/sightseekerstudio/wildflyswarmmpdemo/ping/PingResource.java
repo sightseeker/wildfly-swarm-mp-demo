83552ae0fa72db26ba64f1de425dab15ad229de2
SightSeeker <master@sightseekerstudio.com>
2018-12-13 13:43:38 +0900

    fix ping

--- src/main/java/com/sightseekerstudio/wildflyswarmmpdemo/ping/PingResource.java
+++ src/main/java/com/sightseekerstudio/wildflyswarmmpdemo/ping/PingResource.java
@@ -3,20 +3,19 @@ package com.sightseekerstudio.wildflyswarmmpdemo.ping;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.extensions.Extension;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/ping")
 @Tag(name = "PING")
-@Extension(name = "x-tagGroups", value = "GENERAL")
 public class PingResource {
 
     @GET
-    @Produces("text/plain")
-    @Operation(summary = "ping", description = "疎通確認用リソース")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Operation(summary = "ping", description = "疎通確認用")
     public Response doGet() {
         return Response.ok("pong").build();
     }
